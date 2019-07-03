6a19e798df10528f45878460d7438681f883f478
Arnaud Mignan <arnaudmignan@staff-net-nw-1956.ethz.ch>
2019-07-03 15:54:10 +0200

    Basel dataset updated (bleedoff part removed)

--- R/data.R
+++ R/data.R
@@ -4,11 +4,13 @@
 #' at the Basel, Switzerland, Enhanced Geothermal System. Digitzed (with 1-minute
 #' resolution) from Fig. 5a of Haering M.O., Schanz U., Ladner F., Dyer B.C.
 #' (2008), Characterisation of the Basel 1 enhanced geothermal system. Geothermics,
-#' 37, 469-495, doi: 10.1016/j.geothermics.2008.06.002
+#' 37, 469-495, doi: 10.1016/j.geothermics.2008.06.002 - Post-injection bleed-off
+#' not included.
 #'
-#' @format A data frame with 14755 rows and 2 variables:
+#' @format A data frame with 8229 rows and 2 variables:
 #' \describe{
 #'   \item{t}{time (in decimal days) since injection start at 2006-12-02 18:00:00.00}
+#'   \item{dV}{flow rate (in cubic metres/day) at time t}
 #'   \item{V}{cumulative volume (in cubic metres) injected up to time t}
 #' }
 #' @source \url{https://www.sciencedirect.com/science/article/pii/S0375650508000382}
