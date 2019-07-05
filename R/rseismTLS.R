e36439c239da5bc98ca98efe3964ae31dbcad026
Arnaud Mignan <arnaudmignan0@gmail.com>
2019-07-05 13:38:34 +0200

    init

---
+++ R/rseismTLS.R
@@ -0,0 +1,16 @@
+#' rseismTLS: Induced Seismicity Traffic Light System
+#'
+#' Provides the tools to build a dynamic traffic light system (TLS)
+#' to mitigate the risk of induced seismicity during underground stimulation
+#' by fluid injection.
+#'
+#' @section data
+#' "Basel2006_inj", "Basel2006_seism"
+#' @section rseismTLS forecast functions:
+#' a_fb.val, data.bin, poi.loglik, ratemodel.val, ratemodel.loglik, ratemodel.mle
+#' @section rseismNet risk functions:
+#'
+#' @docType package
+#' @name rseismTLS
+
+NULL
